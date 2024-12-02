ab92378872222d2f77ece5cd76d83d64094cff87
SCOPE\lab307erb <joeho0706@gmail.com>
2024-12-02 11:47:20 +0800

    add fields of /validators/users.js and /models/user.js

--- api/models/user.js
+++ api/models/user.js
@@ -4,9 +4,16 @@ const bcrypt = require('bcrypt');
 // 使用者資料模式
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, sparse: true },
+    password: { type: String, required: true, sparse: true },
+    email: { type: String, required: true, unique: true, sparse: true },
+
+    googleId: { type: String },
+    facebookId: { type: String },
+    registerDate: { type: Date, default: Date.now },
+    name: { type: String, required: true },
+    thumbnail: { type: String },
+    role: { type: String, required: true, default: 'user' },
   },
   {
     timestamps: true, // 自動添加 createdAt 和 updatedAt 字段
@@ -20,5 +27,10 @@ userSchema.pre('save', async function (next) {
   }
   next();
 });
+
+// 驗證密碼
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
--- api/validators/user.js
+++ api/validators/user.js
@@ -9,6 +9,23 @@ const userValidationRules = () => {
       .isLength({ min: 5 })
       .withMessage('Password must be at least 5 characters long'),
     body('email').isEmail().withMessage('Email must be valid'),
+
+    body('googleId')
+      .isLength({ min: 3 })
+      .withMessage('Google ID must be at least 3 characters long'),
+    body('facebookId')
+      .isLength({ min: 3 })
+      .withMessage('Facebook ID must be at least 3 characters long'),
+    body('registerDate')
+      .isDate()
+      .withMessage('Register Date must be a valid date'),
+    body('name')
+      .isLength({ min: 3 })
+      .withMessage('Name must be at least 3 characters long'),
+    body('thumbnail')
+      .isLength({ min: 3 })
+      .withMessage('Thumbnail must be at least 3 characters long'),
+    body('role').notEmpty().withMessage('Role is required'),
   ];
 };
 
