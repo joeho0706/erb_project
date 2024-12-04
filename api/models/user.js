11379fcf28546613a2c0bf69326f3252d7033974
LAPTOP-H2C8BOAC\joeho <joeho0706@gmail.com>
2024-12-05 02:31:18 +0800

    review user schema of loginMethod

--- api/models/user.js
+++ api/models/user.js
@@ -4,15 +4,59 @@ const bcrypt = require('bcrypt');
 // 使用者資料模式
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, min :{ value: 3, message: 'Username must be at least 3 characters long, but got {VALUE}' }, required: true, unique: true, sparse: true,immutable: true },
-    password: { type: String, min :{ value: 5, message: 'Password must be at least 5 characters long, but got {VALUE}' }, required: true, sparse: true },
-    email: { type: String, required: true, unique: true, validate: { validator: function (v) { return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v); }, message: props => `${props.value} is not a valid email address!` }, immutable: true},
+    username: {
+      type: String,
+      min: {
+        value: 3,
+        message: 'Username must be at least 3 characters long, but got {VALUE}',
+      },
+      required: true,
+      unique: true,
+      sparse: true,
+      immutable: true,
+    },
+    password: {
+      type: String,
+      min: {
+        value: 5,
+        message: 'Password must be at least 5 characters long, but got {VALUE}',
+      },
+      required: true,
+      sparse: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+      immutable: true,
+    },
 
     socialMediaId: { type: String, immutable: true },
-    name: { type: String, min :{ value: 3, message: 'Name must be at least 3 characters long, but got {VALUE}' }, required: true },
+    name: {
+      type: String,
+      min: {
+        value: 3,
+        message: 'Name must be at least 3 characters long, but got {VALUE}',
+      },
+      required: true,
+    },
     thumbnail: { type: String },
     role: { type: String, required: true, default: 'user' },
-    loginMethod: { type: String, required: true, enum: { values: ['facebook', 'google', 'local'], message: 'Login method must be one of: facebook, google, local' }},
+    loginMethod: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['facebook', 'google', 'local'],
+        message: 'Login method must be one of: facebook, google, local',
+      },
+      default: 'local',
+    },
   },
   {
     timestamps: true, // 自動添加 createdAt 和 updatedAt 字段
